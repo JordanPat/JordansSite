2addcf3077d0ccee626715cf1099cb5dc513ae38
JordanPat <jordanpbarrilleaux@gmail.com>
2020-01-27 22:06:04 -0600

    added .gitignore

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+
+const Index = () => (
+
+    <div style={StyleSheet.container}>
+        <p>Hello World</p>
+    </div>
+
+    // <style>{`
+
+    //     container,main{
+    //         alig:center,
+    //     }
+
+    // `}</style>
+
+);
+
+
+export default Index;
+
