d6e60ac20e30c7375968a4ff61a7428bf9fc589d
JordanPat <jordanpbarrilleaux@gmail.com>
2024-03-05 11:59:01 -0600

    updated CV link

--- src/pages/CV.tsx
+++ src/pages/CV.tsx
@@ -11,7 +11,7 @@ interface CVProps {
 const CV: React.FC<CVProps> = (CVProps) => {
     return(
         <>
-            <iframe title="Resume" src="https://docs.google.com/document/d/1xosWtN73FfCNJIV9iYffQTd7239j7MtDjYvymvS3x3g/edit?usp=sharing" frameBorder="1" style={{ display:"block", width:"100%", height:"1000px"}}>IFRAME not supported!</iframe>
+            <iframe title="Resume" src="https://docs.google.com/document/d/e/2PACX-1vT_KO1q3L_Pd7GvuF7nQeYnNuwBa4GQAWeyLYhzHqUqgvOphz8zAahEZta7JMgtXP0I_tyJv1_96MFT/pub?embedded=true" frameBorder="1" style={{ display:"block", width:"100%", height:"1000px"}}>IFRAME not supported!</iframe>
         </>
     );
 }
